2ee74a6d805b63318ffecb120fde488f2e661aa7
billryan <yuanbin2014@gmail.com>
2019-03-26 23:53:27 +0800

    refactor task builder module

--- src/main/java/me/yuanbin/kafka/KafkaStreamRouter.java
+++ src/main/java/me/yuanbin/kafka/KafkaStreamRouter.java
@@ -20,26 +20,30 @@ public class KafkaStreamRouter {
     private static final String APPLICATION_ID = "kafka.application.id";
     private static final String BOOTSTRAP_SERVERS = "kafka.bootstrap.servers";
     private static final Config appConfig = AppConfigFactory.load();
+    private static final StreamsBuilder streamsBuilder = new StreamsBuilder();
 
     public static void main(final String[] args) throws Exception {
 
-        StreamsBuilder builder = new StreamsBuilder();
-        buildTask(builder, new MaxwellKeyTask());
+        buildTask(new MaxwellKeyTask());
+        startTasks();
+    }
 
-        KafkaStreams streams = new KafkaStreams(builder.build(), getKafkaProps());
+    private static void buildTask(AbstractTask task) {
+        logger.info("build task with class {}", task.getClass().getSimpleName());
+        task.build(streamsBuilder);
+    }
+
+    private static void startTasks() {
+        String appId = appConfig.getString(APPLICATION_ID);
+        String servers = appConfig.getString(BOOTSTRAP_SERVERS);
+        KafkaStreams streams = new KafkaStreams(streamsBuilder.build(), getKafkaProps(appId, servers));
         streams.start();
     }
 
-    private static void buildTask(StreamsBuilder builder, AbstractTask task) {
-        task.init(builder);
-    }
-
-    private static Properties getKafkaProps() {
+    protected static Properties getKafkaProps(String appId, String servers) {
         Properties props = new Properties();
-        String appId = appConfig.getString(APPLICATION_ID);
-        String bootstrapServers = appConfig.getString(BOOTSTRAP_SERVERS);
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, appId);
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         return props;
--- src/test/java/me/yuanbin/kafka/KafkaStreamRouterTest.java
+++ src/test/java/me/yuanbin/kafka/KafkaStreamRouterTest.java
@@ -1,15 +1,20 @@
 package me.yuanbin.kafka;
 
-import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
 import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
 
 public class KafkaStreamRouterTest {
 
     @Test
-    public void testConfig() {
-        System.out.println("Serdes String class: " + Serdes.String().getClass());
-
-//        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-//        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    public void testProperties() {
+        String appId = "application-id";
+        String servers = "127.0.0.1:9092";
+        Properties properties = KafkaStreamRouter.getKafkaProps(appId, servers);
+        assertEquals(properties.getProperty(StreamsConfig.APPLICATION_ID_CONFIG), appId);
+        assertEquals(properties.getProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG), servers);
     }
 }
