9cf447591bf277a4aa6903320b17966aed755869
billryan <yuanbin2014@gmail.com>
2019-04-07 22:46:25 +0800

    add abstract task

--- src/main/java/me/yuanbin/kafka/task/AbstractTask.java
+++ src/main/java/me/yuanbin/kafka/task/AbstractTask.java
@@ -1,67 +1,48 @@
 package me.yuanbin.kafka.task;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import me.yuanbin.common.config.AppConfigFactory;
 
-import com.google.common.collect.ImmutableList;
 import com.typesafe.config.Config;
-import com.typesafe.config.ConfigException;
-import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.connect.json.JsonSerializer;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static me.yuanbin.common.util.TypesafeConfigUtil.getStringList;
 
 public abstract class AbstractTask {
 
     private static final Logger logger = LoggerFactory.getLogger(AbstractTask.class);
 
     private static final String SOURCE_TOPICS = "source.topics";
-    private static final String SINK_TOPICS = "sink.topics";
-    protected static final String KEY_FILTER_REGEX = "key.filter_regex";
-    protected static final String VALUE_FILTER_REGEX = "value.filter_regex";
+    private static final String SINK_TOPIC_ROUTER = "sink.topic.router";
+    protected static final String WHITELIST = "whitelist";
+    protected static final String ENABLE_WHITELIST_REGEX = "enableWhitelistRegex";
 
     private static final Config appConfig = AppConfigFactory.load();
     protected final Config taskConfig = appConfig.getConfig("task." + this.getClass().getSimpleName());
     protected final List<String> sourceTopics = getStringList(taskConfig, SOURCE_TOPICS);
-    protected final Config sinkTopicsConfig = taskConfig.getConfig(SINK_TOPICS);
+    protected final Config sinkTopicRouter = taskConfig.getConfig(SINK_TOPIC_ROUTER);
 
-    /**
-     * get string list from given path
-     * @param config TypesafeConfig instance
-     * @param path relative path in config
-     * @return list of string
-     */
-    protected List<String> getStringList(Config config, String path) {
-        List<String> sourceTopics = ImmutableList.of();
-        if (taskConfig.hasPath(path)) {
-            try {
-                sourceTopics = ImmutableList.copyOf(config.getStringList(path));
-            } catch (ConfigException.WrongType ex) {
-                logger.warn("path {} is not list, get string instead...", path);
-                sourceTopics = ImmutableList.of(config.getString(path));
-            }
-        }
-        return sourceTopics;
-    }
-
-    /**
-     * get regex string from given path,
-     * .* by default if empty, concatenate list of strings
-     * @param config TypesafeConfig instance
-     * @param path relative path in config
-     * @return regex string
-     */
-    protected String getRegex(Config config, String path) {
-        List<String> regexList = getStringList(config, path);
-        if (regexList.size() == 0) {
-            return ".*";
-        } else if (regexList.size() == 1) {
-            return regexList.get(0);
-        } else {
-            return regexList.stream().collect(Collectors.joining("|", "(", ")"));
-        }
-    }
-
-    public abstract void build(StreamsBuilder builder);
+    // JSON untyped Serde
+    private final Serde<JsonNode> jsonSerde = Serdes.serdeFrom(new JsonSerializer(), new JsonDeserializer());
+    protected final Consumed<JsonNode, JsonNode> jsonConsumed = Consumed.with(jsonSerde, jsonSerde);
+    protected final Produced<JsonNode, JsonNode> jsonProduced = Produced.with(jsonSerde, jsonSerde);
+    // old kafka 0.8.2 does not have timestamp, use process time instead
+    protected final Consumed<String, JsonNode> sensorsConsumed = Consumed.with(Serdes.String(), jsonSerde)
+            .withTimestampExtractor(new WallclockTimestampExtractor());
+    protected final Consumed<String, JsonNode> stringJsonConsumed = Consumed.with(Serdes.String(), jsonSerde);
+    protected final Produced<String, JsonNode> stringJsonProduced = Produced.with(Serdes.String(), jsonSerde);
+    protected final Consumed<JsonNode, byte[]> jsonBytesConsumed = Consumed.with(jsonSerde, Serdes.ByteArray());
+    protected final Consumed<byte[], byte[]> bytesConsumed = Consumed.with(Serdes.ByteArray(), Serdes.ByteArray());
+    protected final Produced<JsonNode, byte[]> jsonBytesProduced = Produced.with(jsonSerde, Serdes.ByteArray());
+    protected final Produced<byte[], byte[]> bytesProduced = Produced.with(Serdes.ByteArray(), Serdes.ByteArray());
 }
