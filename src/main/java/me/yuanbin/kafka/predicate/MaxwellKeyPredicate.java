5981d7027eecf5fffe096d258d52dc0c567c3d69
billryan <yuanbin2014@gmail.com>
2019-04-07 22:45:19 +0800

    refactor MaxwellKeyRouterTask

--- src/main/java/me/yuanbin/kafka/predicate/MaxwellKeyPredicate.java
+++ src/main/java/me/yuanbin/kafka/predicate/MaxwellKeyPredicate.java
@@ -3,16 +3,29 @@ package me.yuanbin.kafka.predicate;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.kafka.streams.kstream.Predicate;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class MaxwellKeyPredicate implements Predicate<JsonNode, byte[]> {
 
     private static final String DATABASE = "database";
     private static final String TABLE = "table";
-    private final Pattern pattern;
+    private Set<String> whitelistDbTable;
+    private boolean regexMode;
+    private Pattern pattern;
 
-    public MaxwellKeyPredicate(String dbRegex) {
-        this.pattern = Pattern.compile(dbRegex);
+    public MaxwellKeyPredicate(List<String> whitelistDbTable, boolean regexMode) {
+        this.whitelistDbTable = new HashSet<>(whitelistDbTable);
+        this.regexMode = regexMode;
+        if (regexMode) {
+            pattern = Pattern.compile(String.join("|", this.whitelistDbTable));
+        }
+    }
+
+    public MaxwellKeyPredicate(List<String> whitelistDbTable) {
+        this(whitelistDbTable, false);
     }
 
     public boolean test(JsonNode key, byte[] value) {
@@ -21,7 +34,12 @@ public class MaxwellKeyPredicate implements Predicate<JsonNode, byte[]> {
         }
         String database = key.get(DATABASE).asText();
         String table = key.get(TABLE).asText();
-        String dbTable = database + "." + table;
-        return pattern.matcher(dbTable).matches();
+        String dbTable = String.format("%s.%s", database, table);
+        if (regexMode) {
+            return pattern.matcher(dbTable).matches();
+        } else {
+            String dbAllTable = String.format("%s.%s", database, "*");
+            return whitelistDbTable.contains(dbTable) || whitelistDbTable.contains(dbAllTable);
+        }
     }
 }
---
+++ src/main/java/me/yuanbin/kafka/task/impl/MaxwellKeyRouterTask.java
@@ -0,0 +1,40 @@
+package me.yuanbin.kafka.task.impl;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigValue;
+import me.yuanbin.common.util.TypesafeConfigUtil;
+import me.yuanbin.kafka.predicate.MaxwellKeyPredicate;
+import me.yuanbin.kafka.task.AbstractTask;
+import me.yuanbin.kafka.task.ETLTask;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+
+public class MaxwellKeyRouterTask extends AbstractTask implements ETLTask {
+
+    private static final Logger logger = LoggerFactory.getLogger(MaxwellKeyRouterTask.class);
+
+    @Override
+    public void build(StreamsBuilder builder) {
+        final KStream<JsonNode, byte[]> sourceStream = builder.stream(sourceTopics, jsonBytesConsumed);
+
+        for (Map.Entry<String, ConfigValue> entry : sinkTopicRouter.entrySet()) {
+            // TypesafeConfig would make xxx.xxx as a unique key, so we should split it with .
+            String rawSinkTopic = entry.getKey();
+            String sinkTopic = rawSinkTopic.split("\\.")[0];
+            logger.info("build stream task with sink.topic {}", sinkTopic);
+            Config sinkTopicConfig = sinkTopicRouter.getConfig(sinkTopic);
+            List<String> whitelist = TypesafeConfigUtil.getStringList(sinkTopicConfig, WHITELIST);
+            boolean enableWhitelistRegex = sinkTopicConfig.hasPath(ENABLE_WHITELIST_REGEX)
+                    && sinkTopicConfig.getBoolean(ENABLE_WHITELIST_REGEX);
+            sourceStream
+                    .filter(new MaxwellKeyPredicate(whitelist, enableWhitelistRegex))
+                    .to(sinkTopic, jsonBytesProduced);
+        }
+    }
+}
